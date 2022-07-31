e1c181be211c20dbd27b022c4e5b64f06f6b6823
huanglshu <huanglshu@qq.com>
2022-07-31 21:39:21 +0800

    重写getMetrics 方法

--- refactoring-first/src/main/java/com/example/JobMetricService.java
+++ refactoring-first/src/main/java/com/example/JobMetricService.java
@@ -2,53 +2,67 @@ package com.example;
 
 import java.time.ZonedDateTime;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 import java.util.function.Function;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class JobMetricService {
+
+
+
     public List<JobMetric> getMetrics() {
         List<JobMetric> result = new ArrayList<>();
         for (Type type : Type.values()) {
             List<SubType> subTypes = getSubTypes(type);
             List<Area> areas = getAreas(type);
             if (subTypes.isEmpty()) {
-              subTypes.add(SubType.NULL);
-//                if (areas.isEmpty()) {
-//                    JobMetric jobMetric = getMetric(type, Optional.empty(), Optional.empty());
-//                    result.add(jobMetric);
-//                } else {
-//                    for (Area area : areas) {
-//                        JobMetric jobMetric = getMetric(type, Optional.empty(), Optional.of(area));
-//                        result.add(jobMetric);
-//                    }
-//                }
+                if (areas.isEmpty()) {
+                    JobMetric jobMetric = getMetric(type, Optional.empty(), Optional.empty());
+                    result.add(jobMetric);
+                } else {
+                    for (Area area : areas) {
+                        JobMetric jobMetric = getMetric(type, Optional.empty(), Optional.of(area));
+                        result.add(jobMetric);
+                    }
+                }
             }
-
-            subTypes.stream().forEach(subType -> {
-              if (subType == SubType.NULL){
-                result.addAll(getJobMetrics(type, Optional.empty()).apply(areas));
-              } else {
-                result.addAll(getJobMetrics(type, Optional.of(subType)).apply(areas));
-              }
-            });
-
-
-//            else {
-//                for (SubType subType : subTypes) {
-//                    if (areas.isEmpty()) {
-//                        JobMetric jobMetric = getMetric(type, Optional.of(subType), Optional.empty());
-//                        result.add(jobMetric);
-//                    } else {
-//                        for (Area area : areas) {
-//                            JobMetric jobMetric = getMetric(type, Optional.of(subType), Optional.of(area));
-//                            result.add(jobMetric);
-//                        }
-//                    }
-//                }
-//            }
+            else {
+                for (SubType subType : subTypes) {
+                    if (areas.isEmpty()) {
+                        JobMetric jobMetric = getMetric(type, Optional.of(subType), Optional.empty());
+                        result.add(jobMetric);
+                    } else {
+                        for (Area area : areas) {
+                            JobMetric jobMetric = getMetric(type, Optional.of(subType), Optional.of(area));
+                            result.add(jobMetric);
+                        }
+                    }
+                }
+            }
         }
+        return result;
+    }
+
+
+    public List<JobMetric> getMetricsNew() {
+        List<JobMetric> result = new ArrayList<>();
+        Arrays.stream(Type.values()).forEach(type -> {
+            List<SubType> subTypes = getSubTypes(type);
+            List<Area> areas = getAreas(type);
+            if (subTypes.isEmpty()) {
+                subTypes.add(SubType.NULL);
+            }
+            subTypes.stream().map(subType -> {
+                Optional optional = Optional.empty();
+                if (subType != SubType.NULL) {
+                    optional = Optional.of(subType);
+                }
+                return getJobMetrics(type, optional).apply(areas);
+            }).forEach(jobMetrics -> result.addAll(jobMetrics));
+        });
         return result;
     }
 
@@ -88,4 +102,6 @@ public class JobMetricService {
                 .area(area.isPresent() ? area.get().toString() : "ALL")
                 .build();
     }
+
+
 }
